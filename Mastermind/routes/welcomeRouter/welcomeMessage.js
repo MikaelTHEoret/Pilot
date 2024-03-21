e3b3b72482c429d08138d6d554977d9cf28e7188
MikaelTHEoret <162789773+MikaelTHEoret@users.noreply.github.com>
2024-03-21 18:02:43 -0400

    Delete Mastermind/routes directory

--- Mastermind/routes/welcomeRouter/welcomeMessage.js
+++
@@ -1,7 +0,0 @@
-// welcomeMessage.js
-
-// Define the welcome message
-const welcomeMessage = 'Welcome to Mastermind App!';
-
-// Export the welcome message for use in the application
-module.exports = welcomeMessage;
